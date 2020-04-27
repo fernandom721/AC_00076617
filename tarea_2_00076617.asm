1e9a10f781866b0b630878c7a19a1f236118618f
fernandom721 <00076617@uca.edu.sv>
2020-04-26 20:56:44 -0600

    ejercicio 2

--- tarea_2_00076617.asm
+++ tarea_2_00076617.asm
@@ -1,7 +1,28 @@
         org 100h
 
 section .text
+        ;ejercicio 2
+        mov     ax, 2d
+        mov     bx, 0h
+        mov     cx, 0000h
+        mov     dx, 0000h
         
+        mov     cx, 0h 
+        mul     cx
+        mov     [210h], ax
+
+loop11:   inc     bx
+        mul     cx
+        mov     [bx+210h], ax
+        cmp     ax, 0xff
+        jb      loop11
+
+loop12: inc    bx
+        inc     bx
+        mul     cx
+        mov     [bx+210h],ax
+        cmp     bx, 12d
+        jb      loop12
 
 
         ;ejercicio 1
